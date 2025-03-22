00f894492ac8f0f8917fa1e4092e7d6481c6f272
Nirmal8501 <nirmal.jadhao@gmail.com>
2025-03-22 23:13:02 +0530

    initial commit

---
+++ src/main/java/com/nirmal/NotesApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nirmal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesApiServiceApplication.class, args);
+	}
+
+}
